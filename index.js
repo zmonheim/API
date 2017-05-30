f6dee8786eba479b018137f01b252ffd567d150d
zmonheim <zmonheim@ncarb.org>
2017-05-30 13:15:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 Console.log("zawad is childish af");
+
+var express = require('express');
+var path = require('path');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = module.exports = express();
+
