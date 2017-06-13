76600187bb87214a71722b55d62ba604635f4dcf
zmonheim <zmonheim@ncarb.org>
2017-06-13 15:13:07 -0400

    Create index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 var pg = require('pg');
@@ -10,6 +9,13 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+
+
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
@@ -23,14 +29,6 @@ app.get('/db', function (request, response) {
   });
 });
 
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
 
 app.get('/message', function(request, response){
   var mess = "Access to this database is offline.   :("
@@ -113,25 +111,6 @@ app.get('/motivation', function(request, response){
   response.send(messages[i]);
 });
 
-app.get('/sound', function(request, response) {
-  //response.send("sound");
-  this.onload = function (){
-    var self = new webkitAudioContext("cow_moo1")
-    //self.location="cow_moo1.wav"
-    //var sound = new (window.AudioContext)
-    self.play();
-  };
-});
-
-
-
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i = 0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
