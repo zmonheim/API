879289fc2864b8b2f0f2790a945846c6250420dd
zmonheim <zmonheim@ncarb.org>
2017-06-13 10:43:42 -0400

    Create index.js

--- index.js
+++ index.js
@@ -86,34 +86,6 @@ app.post('/message', function(request, response){
 
 })
 
-app.get('/fakework', function(request, response){
-  var other = "var express = require('express'); \nvar path = require('path');\nvar session = require('express-session');\nvar bodyParser = require('body-parser');\nvar methodOverride = require('method-override');\n\tapp.set('views', path.join(__dirname, 'views'));\n\tapp.set('view engine', 'jade');\n\n\tapp.use(logger('dev')); \n\tapp.use(bodyParser.json());";
-  var jaunt = "\nreach(main.loop) = new [] <String>();\n---------------------------------------------------------------\n...complete inventory...\n\tapp.create(ncarb-messages);\n{([{([O])}])}\n---> 1+1=2 <--- //according to Carbo IslaMonheim Equation\nvar int1, int2;\nsum(int1, int2);\n\tassert sum(1, 1) = 2;\n\nif (V=IR)\n\tapp.set('voltage equals current multiplied by resistance'); \n___________________________________________________________________\n\trun.app.ncarb-messsages\n\t\treturn = \"Error accessing database\"";
-  var dank = "\n\nrelay...\n******************start correlation********************\n|| && || = {/.}\nvar bool\nif (console.check(eiffelDatabase))\n\tbool = true;\nelse\n\tbool = false;\nsystem.out.println('Zawad is a fool');\n31@438$83918#\n\ntransfering.... \n\tcompiling database --n eiffelDatabase;\nreturn() \ninstall suh dood-- XX !!ERROR!! declined\napp.use(heroku('express'));\n==============================================================\n01001010 01110111 01011011 01000111\n\n";
-  var random = "♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦\nvar under = resist('underdog');\n\t{CRUD}\n Create == Instatiate API\n Read == GET message\n Update == Maintain app \n Delete == TERMINATE\n\n"
-  response.send(random+other+dank+jaunt);
-});
-
-app.get('/zawad', function(request, response){
-  var msg = ("Zawad ")
-  var msg1 = ("Is Childish")
-  var msg2 = ("Islam")
-  var msg3 = ("Islame")
-  var msg4 = ("Is Brown")
-  var msg5 = ("Is An Uncooked Bagel")
-  var msg6 = ("Was Born In Bangladesh")
-  var msg7 = ("Is A Wyntern")
-  var msg8 = ("Is A Bad Jaunt")
-  var msg9 = ("Is Grinding")
-  var msg10 = ("Is Grinding On Another Dude")
-  var msg11 = ("Was Born In Queens ")
-  var msg12 = ("Is The GOAT")
-  var msg13 = ("Looks Like A Goat")
-  var messages = [msg1, msg2, msg3, msg4, msg5, msg6, msg7, msg8, msg9, msg10, msg11, msg12, msg13]
-  var i = Math.floor(Math.random()*13)
-  response.send(msg + messages[i]);
-});
-
 app.get('/motivation', function(request, response){
   var msg1 = ("You miss 100% of the shots you don't take.")
   var msg2 = ("Sometimes the thing that is holding you back is all in your head.")
